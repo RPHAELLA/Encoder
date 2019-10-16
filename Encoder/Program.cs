0c6569c40438d446bf7a0f0cbf6f19bc1fb33b03
RPHAELLA <s0da.kissmatic@gmail.com>
2019-10-16 12:02:46 +0800

    Initial commit

---
+++ Encoder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Source code directory (full path): ");
+            Console.Out.Flush();
+            var location = Console.ReadLine();
+            Console.Write("File extension (e.g. .cs): ");
+            Console.Out.Flush();
+            var extension = Console.ReadLine();
+
+            // Convert to UTF-8
+            /*
+             * Example of location: @"C:\Users\X\Desktop\Source")
+             * Example of extension: *.cpp
+             * 
+             */
+            foreach (var f in new DirectoryInfo(location).GetFiles("*" + extension, SearchOption.AllDirectories))
+            {
+                string s = File.ReadAllText(f.FullName);
+                Console.WriteLine("[*] Converting file: " + f.FullName);
+                File.WriteAllText(f.FullName, s, Encoding.UTF8);
+            }
+            Console.WriteLine("Operation completed");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,24 @@
+# Encoder
+
+> A source code encoding conversion tool
+
+This project allows the conversion of source code from any encoding to UTF-8.
+
+## Usage
+
+To run the script, download and execute the `.exe` file. 
+
+Alternatively, download the project and build the the console script locally. 
+
+#### Run .\Encoder.exe
+
+The script will prompt for two inputs, namely the **project directory** and the **specific extension of files** to lookout for.  
+
+#### Thank you
+
+While working with FlawFinder, I was hit with the following error:
+
+> *UnicodeDecodeError: 'utf-8' codec can't decode byte ... in position ...: invalid continuation byte* 
+>
+
+To perform mass source code encoding conversion to UTF-8 for a client's project, the script was just a quick spin up a reference to an answer on StackOverflow written by [Timwi](https://stackoverflow.com/users/33225/timwi) and [Ivan Perevezentsev](https://stackoverflow.com/users/4027465/ivan-perevezentsev). 
